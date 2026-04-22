03f8dd7b2fff98f7e44d9a6b016456dfd58dbc84
Yann-26 <yannassiri26@gmail.com>
2026-04-22 11:30:22 +0530

    .

--- litconnect/settings.py
+++ litconnect/settings.py
@@ -128,26 +128,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-
-# AWS S3 Settings
-AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-AWS_S3_REGION_NAME = os.environ.get(
-    "AWS_S3_REGION_NAME", "us-east-1"
-)  # e.g., 'us-east-1'
-
-# Tell Django to use S3 for uploaded files
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
-# If you want S3 to also host your CSS/JS (optional)
-# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
-
-# URL configuration
-AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
-MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
-
-
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
 # CRITICAL FIX: Set STATIC_ROOT
@@ -170,3 +150,12 @@ SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
 
 # Security settings for production
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_HSTS_SECONDS = 31536000  # 1 year
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
